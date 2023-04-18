c9bd207bf4a443e4c782c74e52380125bb72d0a7
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-04-19 01:53:17 +0300

    Adds a task-03 solution

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const { delay, step, amount } = evt.currentTarget.elements;
+
+  let delayValue = Number(delay.value);
+  const stepValue = Number(step.value);
+  const amountValue = Number(amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    const promise = createPromise(i, delayValue);
+
+    promise
+      .then(value => {
+        Notify.success(value, { useIcon: false });
+      })
+
+      .catch(error => {
+        Notify.failure(error, { useIcon: false });
+      });
+
+    delayValue += stepValue;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        // Reject
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
